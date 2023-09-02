5f6ea4f004155eecdd3e3ecafc225678ef82bda2
Miguel Bohórquez <dlsg1009@gmail.com>
2023-09-02 17:20:22 -0500

    Agregando archivo productos.js

---
+++ productos.js
@@ -0,0 +1,21 @@
+let productos = [{
+    nombre: "Hamburguesa simple",
+    costo: 20,
+    codigo: "H100"
+},
+{
+    nombre: "Hamburguesa doble",
+    costo: 30,
+    codigo: "H200"
+},
+{
+    nombre: "Papas fritas",
+    costo: 8,
+    codigo: "P100"
+},
+{
+    nombre: "Refresco personal",
+    costo: 6,
+    codigo: "R100"
+}
+]
